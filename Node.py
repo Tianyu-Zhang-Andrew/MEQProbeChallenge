82082d33df1a75661e733f14589cf2e4d6a90b69
Tiany <TianyuZhangAndrew@outlook.com>
2023-04-11 18:55:55 +1000

    Add Readme

--- Node.py
+++ Node.py
@@ -2,7 +2,6 @@
 class Node:
     def __init__(self, value):
         self.__value = value
-        # self.__neighbors = []
         self.__explored_count = 0
 
     def get_value(self):
@@ -10,22 +9,6 @@ class Node:
 
     def set_value(self, value):
         self.__value = value
-
-    # def has_neighbor(self, neighbor_value):
-    #     for neighbor in self.__neighbors:
-    #         if neighbor.get_value() == neighbor_value:
-    #             return True
-    #
-    #     return False
-
-    # def add_neighbor(self, neighbor):
-    #     self.__neighbors.append(neighbor)
-    #
-    # def set_neighbors(self, neighbors):
-    #     self.__neighbors = neighbors
-    #
-    # def get_neighbors(self):
-    #     return self.__neighbors
 
     def get_explored_count(self):
         return self.__explored_count
